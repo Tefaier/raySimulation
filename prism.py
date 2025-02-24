0f07af374d4491b527e9b621843d09eac7ab3489
Timofei <timabelov08@gmail.com>
2025-02-24 21:00:37 +0300

    prism fix

--- prism.py
+++ prism.py
@@ -11,10 +11,10 @@ from models.surface import ReflectionSurface, RefractionSurface, SolidSurface, S
 from scene_running import Camera, render_scene
 from utils import sellmeier_lambda
 
-dist_to_mirror = 1
-camera = Camera(np.array([-3.2, -0.1, -0.5], dtype=float),
-                np.array([-3.2, -0.1, 0.5]),
-                np.array([-2.88, -1.04, 0.5]), 50)  # change angle of view
+y_shift = 0.1
+camera = Camera(np.array([-3.2, -0.1 + y_shift, -0.5], dtype=float),
+                np.array([-3.2, -0.1 + y_shift, 0.5]),
+                np.array([-2.88, -1.04 + y_shift, 0.5]), 50)  # change angle of view
 surfaces = []
 
 side_length = 1.5 * 2
@@ -31,15 +31,17 @@ sides_surfaces = [
     *get_triangle_equation(points[1], points[3], points[4]),
     *get_triangle_equation(points[1], points[4], points[2]),
     *get_triangle_equation(points[2], points[4], points[5]),
+    *get_triangle_equation(points[0], points[1], points[2]),
+    *get_triangle_equation(points[4], points[3], points[5])
 ]
 surfaces.append(RefractionSurface(sides_surfaces, lambda alpha: 1, sellmeier_lambda(1.0396, 6000, 0.2318, 20000)))
 #surfaces.append(SolidSurface(sides_surfaces, np.array([1, 1, 1]), 1))
-surfaces.append(SolidSurface(get_sphere_equations(np.array([3.05, -0.57, 0]), 0.3), np.array([1, 1, 1]), 1))
+surfaces.append(SolidSurface(get_sphere_equations(np.array([3.05, -0.57 + y_shift, 0]), 0.3), np.array([1, 1, 1]), 1))
 
 
 # surfaces.append(SolidSurface(get_cube_equations(np.array([0, 0, 80]), 2), 1, 100))
 # surfaces.append(SolidSurface(get_triangle_equation(np.array([-5, 0, 40]), np.array([0, 5, 40]), np.array([5, 0, 40])), 1, 100))
 surfaces.append(SolidSurface(get_sphere_equations(np.array([0, 0, 0]), 100), np.array([1, 1, 1]), 0.2))
 
-color_split = 3
-render_scene(f"prism_{color_split}.png", surfaces, camera, 100, 100, color_split=color_split)
+color_split = 1
+render_scene(f"prism_{color_split}.png", surfaces, camera, 1000, 1000, color_split=color_split)
